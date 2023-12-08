f28ed4103b24729f448358a190952bf806588c49
消逝中的寒光 <2507544221@qq.com>
2023-12-08 23:23:56 +0800

    实现：店铺营业状态设置，引入Redis

---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,36 @@
+package com.sky.controller.user;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("userShopController")
+@Slf4j
+@RequestMapping("/user/shop")
+@Api("店铺接口")
+public class ShopController {
+
+    public static final String key = "SHOP_STATUS";
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 获取店铺营业状态
+     *
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("获取店铺营业状态")
+    public Result<Integer> getStatus() {
+        Integer shopStatus = (Integer) redisTemplate.opsForValue().get(key);
+        return Result.success(shopStatus);
+    }
+}
